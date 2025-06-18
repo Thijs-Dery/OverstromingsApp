8e09a79f8498eb2cb6d53dbb2d89bc7bc827f42f
Thijs-Dery <thijs.dery@student.ehb.be>
2025-06-18 03:10:21 +0200

    user model

---
+++ OverstromingsApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverstromingsApp.Models;
+
+public class User
+{
+    public Guid Id { get; set; }   
+    public string Email { get; set; } = null!;
+    public byte[] PasswordSalt { get; set; } = null!;
+    public byte[] PasswordHash { get; set; } = null!;
+    public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+}
